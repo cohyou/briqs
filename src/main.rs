332311186678555a4f4adf8a7c13db501afffff8
OKAZAKI Yasuo <cohyou@ironoir.io>
2020-01-02 14:15:40 +0900

    add Briq

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+type BriqIndx = u32;
+
+enum Briq {
+    Eval { appl: BriqIndx },
+    Appl { tape: BriqIndx, schm: BriqIndx },
+    Tape(Vec<BriqIndx>),
+    Schm(Vec<BriqIndx>),    
+}
+
 fn main() {
     println!("Hello, world!");
 }
